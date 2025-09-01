55019aa88d28f738deb196652d800b91f62f92c3
Sam7190 <63897154+Sam7190@users.noreply.github.com>
2025-08-31 17:05:46 -0700

    begin analysis

---
+++ vars.py
@@ -0,0 +1,4 @@
+supply_data_path = 'data\RDC_Inventory_Core_Metrics_Zip_History.csv'
+demand_data_path = 'data\RDC_Inventory_Hotness_Metrics_Zip_History.csv'
+
+zip2county = 'data/ZIP_COUNTY_032019.xlsx'
